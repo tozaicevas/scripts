a4f88ea8fbdedcf187af2f49762d0ae47d7fce5d
Tomas <tozaicevas@gmail.com>
2019-11-22 16:25:31 +0100

    added weights to graphGenerator

--- graphGenerator/graphGenerator.js
+++ graphGenerator/graphGenerator.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
 
-const N_ARGUMENT_INDEX = 2;
+const VERTICES_ARGUMENT_INDEX = 2;
+const MAX_EDGES_ARGUMENT_INDEX = 3;
 const FIRST_LINE = "digraph randomDigraph {";
 const LAST_LINE = "}";
 
-const n = process.argv[N_ARGUMENT_INDEX] || 50;
+const n = process.argv[VERTICES_ARGUMENT_INDEX] || 50;
+const maxEdges = process.argv[MAX_EDGES_ARGUMENT_INDEX] || 5;
 const vertices = [...Array(parseInt(n)).keys()];
 const levelMap = {};
 
-const getEdge = (from, to) => `${from} -> ${to};`;
-const edgeToString = (edge) => `\t${edge}`;
+const getEdge = (from, to) => `${from} -> ${to}`;
+const edgeToString = (edge) => `\t${edge};`;
 const edgesToString = (edges) => edges.map(edge => edgeToString(edge)).join('\n');
 
 const getInitialEdges = (vertices) => {
@@ -29,18 +31,26 @@ const getMoreEdges = (vertices) => {
 			const nonCyclicVertices = vertices
 				.filter(j => levelMap[j] <= levelMap[i] && i != j);	
 			const randomEdgesCount = Math.floor(Math.random() * nonCyclicVertices.length);	
+			const minimizedRandomEdgesCount = Math.min(randomEdgesCount, maxEdges);
 			const shuffledNonCyclicVertices = nonCyclicVertices.sort(() => Math.random() - 0.5);	
-			const verticesForNewEdges = shuffledNonCyclicVertices.slice(0, randomEdgesCount);
+			const verticesForNewEdges = shuffledNonCyclicVertices.slice(0, minimizedRandomEdgesCount);
 			return verticesForNewEdges.map(vertice => getEdge(vertice, i));
 		}).flat();
+};
+
+const addWeights = edges => {
+	return edges.map(edge => {
+		const weight = Math.floor(Math.random() * 100) + 1;
+		return `${edge} [label="${weight}"]`;
+	});
 };
 
 const getDotFormattedGraph = () => {
 	const initialEdges = getInitialEdges(vertices);
 	const moreEdges = getMoreEdges(vertices);
-	const initialEdgesDotFormatted = `${edgesToString(initialEdges)}\n`;	
-	const moreEdgesDotFormatted = edgesToString(moreEdges);
-	const fullGraph = `${initialEdgesDotFormatted}${moreEdgesDotFormatted}`;
+	const allEdges = [...initialEdges, ...moreEdges];
+	const allEdgesWithWeights = addWeights(allEdges);
+	const fullGraph = edgesToString(allEdgesWithWeights);
 	return `${FIRST_LINE}\n${fullGraph}\n${LAST_LINE}`;
 }
 
